45a99504516dd1b2b91c6de8f9824787ec49d7b4
Samuel Ribeiro <samuca3004@hotmail.com>
2022-03-04 13:24:11 -0300

    Primeira migração

--- WebVendasMVC/Startup.cs
+++ WebVendasMVC/Startup.cs
@@ -28,7 +28,7 @@ namespace WebVendasMVC
             services.AddControllersWithViews();
 
             services.AddDbContext<WebVendasMVCContext>(options =>
-                    options.UseSqlServer(Configuration.GetConnectionString("WebVendasMVCContext")));
+                    options.UseMySql(Configuration.GetConnectionString("WebVendasMVCContext"), builder => builder.MigrationsAssembly("WebVendasMVC")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
