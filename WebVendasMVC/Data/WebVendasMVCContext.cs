1a4bf0cd73876d2a2df6e3d290b2922438164e43
Samuel Ribeiro <samuca3004@hotmail.com>
2022-03-03 22:37:20 -0300

    Crud

---
+++ WebVendasMVC/Data/WebVendasMVCContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebVendasMVC.Models;
+
+namespace WebVendasMVC.Data
+{
+    public class WebVendasMVCContext : DbContext
+    {
+        public WebVendasMVCContext (DbContextOptions<WebVendasMVCContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebVendasMVC.Models.Department> Department { get; set; }
+    }
+}
