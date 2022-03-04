2d3a51b3533309b39120421ccb1e6315fa19f794
Samuel Ribeiro <samuca3004@hotmail.com>
2022-03-03 21:05:21 -0300

    Add project files.

---
+++ WebVendasMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebVendasMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
